c2b27923cc58ee24d8e11efcce578fdd3a1f7655
dat_Nguyen <dat958734@gmail.com>
2024-10-17 01:18:30 +0700

    upload

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -72,8 +72,9 @@
                             <li @click="logout" class="block border-b p-2 cursor-pointer hover:bg-gray-500 hover:text-white">Logout</li>
                         </ul>
                     </div>
-                    <div v-else id="login" :class="login ?'flex':'hidden'" @click="openLogin" class="fixed flex justify-center items-center z-20 inset-0 bg-black bg-opacity-50 h-[100vh] w-[100vw]">
-                        <div class="overflow-hidden bg-white w-full sm:w-[23rem] z-50">
+                    <div v-else id="login" :class="login ?'flex':'hidden'"  class="fixed flex justify-center items-center z-20 inset-0 bg-black bg-opacity-50 h-[100vh] w-[100vw]">
+                        <div class="overflow-hidden bg-white w-full sm:w-[23rem] relative">
+                            <div @click="openLogin" class="absolute top-0 right-0 p-2"><VueIcon type="mdi" :path="mdiClose" size="30"/></div>
                             <div class="flex w-[300%] transform transition-transform duration-500 ease-out" 
                                 :style="{ transform: recoverPanel === 'login' ? 'translateX(-33.33%)' : 
                                 recoverPanel === 'register' ? 'translateX(0%)' : 
